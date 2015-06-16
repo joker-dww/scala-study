27998fa6cc9467f234f7a6ca83e45a7ed5a5ddd4
joker <joker@i-um.net>
2015-06-16 16:34:50 +0900

    post form process

--- app/controllers/Second.scala
+++ app/controllers/Second.scala
@@ -3,13 +3,16 @@ package controllers
 import com.ium.concerto.manager.{PeopleGroupAssignManager, RecommenderManager}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+
+import play.api.Logger
+import play.api.data.Form
+import play.api.data.Forms.tuple
+import play.api.data.Forms.text
 import play.api.libs.json.Json._
 import play.api.mvc.{Action, Controller}
 
 import java.text.SimpleDateFormat
 import java.util.Date
-
-import views.html.helper.form
 
 @Component
 class Second extends Controller{
@@ -20,6 +23,14 @@ class Second extends Controller{
   @Autowired
   private var statAssignedGroupList: PeopleGroupAssignManager = null
 
+
+  val baseForm = Form(
+    tuple(
+      "dateString" -> text,
+      "" -> text
+    )
+  )
+
   def getStatus = Action {
 
     val result = Map("exampleField2" -> ("Hello world!" + recommenderManager.getRecommenderStatus) )
@@ -27,10 +38,18 @@ class Second extends Controller{
 
   }
 
-  def getStatAssignedGroup(dateString: Option[String]) = Action {
-    val targetDate = dateString match {
-        case None => new SimpleDateFormat("yyyyMMdd").format(new Date())
-        case _ => dateString.toString
+  def getStatAssignedGroup = Action { implicit request =>
+
+    var targetDate = new SimpleDateFormat("yyyyMMdd").format(new Date())
+    //Logger.debug(s"Result=${request.method}")
+    if (request.method == "POST") {
+
+      val values = baseForm.bindFromRequest.data
+      targetDate = values("dateString") match {
+        case "" => targetDate
+        case _ => values("dateString")
+      }
+
     }
 
     var ls = statAssignedGroupList.getStatAssignedGroup(targetDate)
